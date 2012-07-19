4bbe85fc00f36dce9f7772685d6912c253b77df3
destinydriven <kurn.lamontagne@gmail.com>
2012-07-19 19:11:03 -0300

    Fixed indentation and minor cleanup

--- Controller/MixedChartsDemoController.php
+++ Controller/MixedChartsDemoController.php
@@ -12,11 +12,11 @@
  */
 class MixedChartsDemoController  extends HighChartsAppController
 {
-	var $name = 'MixedChartsDemo';
-	var $components = array('HighCharts.HighCharts');
-	var $helpers = array('HighCharts.HighCharts');
-	var $uses = array();
-	var $layout = 'chart.demo';
+	public $name = 'MixedChartsDemo';
+	public $components = array('HighCharts.HighCharts');
+	public $helpers = array('HighCharts.HighCharts');
+	public $uses = array();
+	public $layout = 'chart.demo';
 
 	/**
 	 * HighCharts component
@@ -38,11 +38,11 @@ class MixedChartsDemoController  extends HighChartsAppController
 				
 		$chartData3 = array(
 				array(
-						'name' => 'Chrome',
-                     	'y' => 45.0,
-                     	'sliced' => true,
-                     	'selected' => true
-					),
+					'name' => 'Chrome',
+                     			'y' => 45.0,
+                     			'sliced' => true,
+                     			'selected' => true
+				     ),
 				array('IE', 26.8),
 				array('Firefox', 12.8),
 				array('Safari', 8.5),
@@ -55,34 +55,34 @@ class MixedChartsDemoController  extends HighChartsAppController
 		$chartNameThree = 'Pie Chart';
 		
 		$mychartOne = $this->HighCharts->create
-										 (
-											$chartNameOne,
-											array
-											(
-												'type' => 'line',
-												'exporting' => TRUE
-											)
-										 );
+						 (
+							$chartNameOne,
+							array
+							(
+								'type' => 'line',
+								'exporting' => TRUE
+							)
+						 );
 										 
-	   $mychartTwo = $this->HighCharts->create
-										 (
-											$chartNameTwo,
-											array
-											(
-												'type' => 'column',
-												'exporting' => TRUE
-											)
-										 );
+	   	$mychartTwo = $this->HighCharts->create
+						 (
+							$chartNameTwo,
+							array
+							(
+								'type' => 'column',
+								'exporting' => TRUE
+							)
+						 );
 										
 		$mychartThree = $this->HighCharts->create
-										 (
-											$chartNameThree,
-											array
-											(
-												'type' => 'pie',
-												'exporting' => TRUE
-											)
-										 );
+						 (
+							$chartNameThree,
+							array
+							(
+								'type' => 'pie',
+								'exporting' => TRUE
+							)
+						 );
 
 	
 	   	$this->HighCharts->setChartParams
@@ -90,24 +90,24 @@ class MixedChartsDemoController  extends HighChartsAppController
 				$chartNameOne,
 				array
 				(
-					'renderTo'					=> 'linewrapper',  // div to display chart inside
+					'renderTo'				=> 'linewrapper',  // div to display chart inside
 					'chartWidth'				=> 800,
 					'chartHeight'				=> 600,					
-					'title'						=> 'Monthly Sales Summary - Line',					
+					'title'					=> 'Monthly Sales Summary - Line',					
 					'yAxisTitleText' 			=> 'Units Sold',
-					'xAxisCategories'           => array(
-														'Jan', 
-														'Feb', 
-														'Mar', 
-														'Apr', 
-														'May', 
-														'Jun',
-														'Jul', 
-														'Aug', 
-														'Sep', 
-														'Oct', 
-														'Nov', 
-														'Dec'													
+					'xAxisCategories'           		=> array(
+											'Jan', 
+											'Feb', 
+											'Mar', 
+											'Apr', 
+											'May', 
+											'Jun',
+											'Jul', 
+											'Aug', 
+											'Sep', 
+											'Oct', 
+											'Nov', 
+											'Dec'													
 									              ),	
 					'creditsEnabled'			=> FALSE
 				) 
@@ -118,25 +118,25 @@ class MixedChartsDemoController  extends HighChartsAppController
 				$chartNameTwo,
 				array
 				(
-					'renderTo'					=> 'columnwrapper',  // div to display chart inside
+					'renderTo'				=> 'columnwrapper',  // div to display chart inside
 					'chartWidth'				=> 800,
 					'chartHeight'				=> 600,					
-					'title'						=> 'Monthly Sales Summary - Column',			
+					'title'					=> 'Monthly Sales Summary - Column',			
 					'yAxisTitleText' 			=> 'Y Axis Title Text',
-					'xAxisCategories'           => array(
-														'Jan', 
-														'Feb', 
-														'Mar', 
-														'Apr', 
-														'May', 
-														'Jun',
-														'Jul', 
-														'Aug', 
-														'Sep', 
-														'Oct', 
-														'Nov', 
-														'Dec'													
-									              ),	
+					'xAxisCategories'           		=> array(
+											'Jan', 
+											'Feb', 
+											'Mar', 
+											'Apr', 
+											'May', 
+											'Jun',
+											'Jul', 
+											'Aug', 
+											'Sep', 
+											'Oct', 
+											'Nov', 
+											'Dec'													
+										      ),	
 					'creditsText'  	 			=> 'Example.com',
 					'creditsURL'				=> 'http://example.com'
 				) 
@@ -147,22 +147,22 @@ class MixedChartsDemoController  extends HighChartsAppController
 				$chartNameThree,
 				array
 				(
-					'renderTo'					=> 'piewrapper',  // div to display chart inside
+					'renderTo'				=> 'piewrapper',  // div to display chart inside
 					'chartWidth'				=> 800,
 					'chartHeight'				=> 600,					
-					'title'						=> 'Browser Usage Statistics',					
+					'title'					=> 'Browser Usage Statistics',					
 					'creditsText'  	 			=> 'Example.com',
 					'creditsURL'				=> 'http://example.com'
 				) 
 			);
 		
-        $seriesOne = $this->HighCharts->addChartSeries();
-		$seriesTwo = $this->HighCharts->addChartSeries();
+        	$seriesOne   = $this->HighCharts->addChartSeries();
+		$seriesTwo   = $this->HighCharts->addChartSeries();
 		$seriesThree = $this->HighCharts->addChartSeries();
 		
 		$seriesOne->addName('Tokyo')->addData($chartData1);
 		$seriesTwo->addName('London')->addData($chartData2);
-		$seriesThree->addName('London')->addData($chartData3);
+		$seriesThree->addName('New York')->addData($chartData3);
 						
 		$mychartOne->addSeries($seriesOne);
 		$mychartTwo->addSeries($seriesTwo);
@@ -172,117 +172,7 @@ class MixedChartsDemoController  extends HighChartsAppController
 
 	public function column()
 	{ 
-		$this->LoadModel('Browser');	
-		$chartData = $this->Browser->find('all', array('fields' => array('Browser.name', 'Browser.amount')));
-		
-		//debug($chartData);
-		
-		$chartData = $this->HighCharts->_reformArray($chartData,'Browser');
-		
-		//$chartData =   Set::classicExtract($chartData,'{0}.Browser');
-	              
-		
-		debug($chartData);
-
-				
-		$chartName = 'Column Chart';
-		
-		$mychart = $this->HighCharts->create
-										 (
-											$chartName,
-											array
-											(
-												'type' => 'pie',
-												'exporting' => TRUE
-											)
-										 );
-
-	
-	   $this->HighCharts->setChartParams
-			(
-				$chartName,
-				array
-				(
-					'renderTo'					=> 'columnwrapper',  // div to display chart inside
-					'chartWidth'				=> 800,
-					'chartHeight'				=> 600,
-					'chartMarginTop' 			=> 60,
-					'chartMarginLeft'			=> 90,
-					'chartMarginRight'			=> 30,
-					'chartMarginBottom'			=> 110,
-					'chartSpacingRight'			=> 10,
-					'chartSpacingBottom'		=> 15,
-					'chartSpacingLeft'			=> 0,
-					'chartAlignTicks'			=> FALSE,					
-					//'chartBackgroundColorLinearGradient' 	=> array(0,0,0,300),
-					//'chartBackgroundColorStops'	=> array(array(0,'rgb(217, 217, 217)'),array(1,'rgb(255, 255, 255)')),
-					
-					'title'						=> 'Monthly Sales Summary',
-					'titleAlign'				=> 'left',
-					'titleFloating'				=> TRUE,
-					'titleStyleFont'			=> '18px Metrophobic, Arial, sans-serif',
-					'titleStyleColor'			=> '#0099ff',
-					'titleX'					=> 20,
-					'titleY'					=> 20,
-					
-					'legendEnabled' 			=> TRUE,
-					'legendLayout'				=> 'horizontal',
-					'legendAlign'				=> 'center',
-					'legendVerticalAlign '		=> 'bottom',
-					'legendItemStyle'			=> array('color' => '#222'),
-					'legendBackgroundColorLinearGradient' => array(0,0,0,25),
-					'legendBackgroundColorStops' => array(array(0,'rgb(217, 217, 217)'),array(1,'rgb(255, 255, 255)')),
-
-                    'tooltipEnabled' 			=> FALSE,
-                   // 'tooltipBackgroundColorLinearGradient' => array(0,0,0,50),   // triggers js error
-                   // 'tooltipBackgroundColorStops' => array(array(0,'rgb(217, 217, 217)'),array(1,'rgb(255, 255, 255)')),
-                    
-					//'plotOptionsLinePointStart' => strtotime('-30 day') * 1000,
-					//'plotOptionsLinePointInterval' => 24 * 3600 * 1000,					
-					
-					//'xAxisType' 				=> 'datetime',
-					//'xAxisTickInterval' 		=> 10,
-					//'xAxisStartOnTick' 			=> TRUE,
-					//'xAxisTickmarkPlacement' 	=> 'on',
-					//'xAxisTickLength' 			=> 10,
-					//'xAxisMinorTickLength' 		=> 5,
-					
-					'xAxisLabelsEnabled' 		=> TRUE,
-					'xAxisLabelsAlign' 			=> 'right',
-					'xAxisLabelsStep' 			=> 2,
-					'xAxisLabelsRotation' 		=> -35,
-					'xAxislabelsX' 				=> 5,
-					'xAxisLabelsY' 				=> 20,					
-					
-					//'yAxisMin' 					=> 0,
-					//'yAxisMaxPadding'			=> 0.2,
-					//'yAxisEndOnTick'			=> FALSE,
-					//'yAxisMinorGridLineWidth' 	=> 0,
-					//'yAxisMinorTickInterval' 	=> 'auto',
-					//'yAxisMinorTickLength' 		=> 1,
-					//'yAxisTickLength'			=> 2,
-					//'yAxisMinorTickWidth'		=> 1,
-					
-					
-					'yAxisTitleText' 			=> 'Units',
-					//'yAxisTitleAlign' 			=> 'high',
-					//'yAxisTitleStyleFont' 		=> '14px Metrophobic, Arial, sans-serif',
-					//'yAxisTitleRotation' 		=> 0,
-					//'yAxisTitleX' 				=> 0,
-					//'yAxisTitleY' 				=> -10,
-					//'yAxisPlotLines' 			=> array( array('color' => '#808080', 'width' => 1, 'value' => 0 )),
-					
-					// autostep options
-					'enableAutoStep' => FALSE							
-				) 
-			);
-		
-        $series1 = $this->HighCharts->addChartSeries();
-		
-		$series1->addName('Browser Distribution')->addData($chartData);		
-						
-		$mychart->addSeries($series1);
-		
+	  // TODO	
 	}
 
 }
