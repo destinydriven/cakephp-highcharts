826d397f3a6a4312dec757da171b9919e90008f2
destinydriven <kurn.lamontagne@gmail.com>
2012-07-20 14:20:36 -0400

    Added examples for Stacked and Combination Charts Added support for more chart params

---
+++ Controller/CombinationDemoController.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ *  CakePHP HighCharts Plugin
+ * 
+ * 	Copyright (C) 2012 Kurn La Montagne / destinydriven
+ *	<https://github.com/destinydriven> 
+ * 
+ * 	Multi-licensed under:
+ * 		MPL <http://www.mozilla.org/MPL/MPL-1.1.html>
+ * 		LGPL <http://www.gnu.org/licenses/lgpl.html>
+ * 		GPL <http://www.gnu.org/licenses/gpl.html>
+ */
+class CombinationDemoController extends HighChartsAppController
+{
+	public $name = 'CombinationDemo';
+	public $components = array('HighCharts.HighCharts');
+	public $helpers = array('HighCharts.HighCharts');
+	public $uses = array();
+	public $layout = 'chart.demo';
+
+	public function combo()
+	{
+		$janeData = array(3, 2, 1, 3, 4);
+		$johnData = array(2, 3, 5, 7, 6);
+		$joeData  = array(4, 3, 3, 9, 0);
+		$avgData  = array(3, 2.67, 3, 6.33, 3.33);
+		
+		$pieData = array(
+				array(
+					'name' => 'Jane',
+		                     	'y' => 13,
+		                     	'sliced' => true,
+		                     	'selected' => true
+				     ),
+				array('John', 23),
+				array('Joe', 19)
+			);
+				
+		$chartName = 'Combination Chart';
+		
+		$mychart = $this->HighCharts->create
+						 (
+							$chartName,
+							array
+							(
+								'type' => 'column',
+								'exporting' => TRUE
+							)
+						 );
+
+	
+	        $this->HighCharts->setChartParams
+			(
+				$chartName,
+				array
+				(
+					'renderTo'				=> 'combowrapper',  // div to display chart inside				
+					'title'					=> 'Combination Chart',
+					'subtitle'				=> 'Source: World Bank',
+					'xAxisLabelsEnabled' 	=> TRUE,				
+					'xAxisCategories'       => array( 'Apples','Oranges','Pears','Bananas','Plums'),				
+					'yAxisTitleText' 		=> 'Units',									
+					'enableAutoStep' 		=> FALSE,
+					'creditsEnabled'		=> FALSE						
+				) 
+			);
+		
+        $janeSeries = $this->HighCharts->addChartSeries();
+		$janeSeries->type = 'column';
+		$janeSeries->addName('Jane')->addData($janeData);
+		
+		$johnSeries = $this->HighCharts->addChartSeries();
+		$johnSeries->type = 'column';
+		$johnSeries->addName('John')->addData($johnData);		
+		
+		$joeSeries  = $this->HighCharts->addChartSeries();
+		$joeSeries->type = 'column';
+		$joeSeries->addName('Joe')->addData($joeData);		
+		
+		$avgSeries  = $this->HighCharts->addChartSeries();
+		$avgSeries->type = 'spline';
+		$avgSeries->addName('Average')->addData($avgData);
+		
+		$pieSeries  = $this->HighCharts->addChartSeries();
+		$pieSeries->type = 'pie';
+		$pieSeries->center = array(200,50);
+		$pieSeries->size = 150;
+		$pieSeries->showInLegend = FALSE;
+		$pieSeries->addName('Total consumption')->addData($pieData);	
+
+		$mychart->addSeries($janeSeries);
+		$mychart->addSeries($johnSeries);
+		$mychart->addSeries($joeSeries);
+		$mychart->addSeries($avgSeries);
+		$mychart->addSeries($pieSeries);	
+		
+	}
+	
+}
