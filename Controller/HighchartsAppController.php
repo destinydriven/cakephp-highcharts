ddbbc937d40c328a59e49e3fb10690f42592a065
destinydriven <kurn.lamontagne@gmail.com>
2014-12-12 11:21:13 -0400

    Renamed files to maintain consistent Highchart product name throughout

---
+++ Controller/HighchartsAppController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  CakePHP HighCharts Plugin
+ * 
+ * 	Copyright (C) 2012 Kurn La Montagne / destinydriven
+ *	<https://github.com/destinydriven> 
+ * 
+ * 	Multi-licensed under:
+ * 		MPL <http://www.mozilla.org/MPL/MPL-1.1.html>
+ * 		LGPL <http://www.gnu.org/licenses/lgpl.html>
+ * 		GPL <http://www.gnu.org/licenses/gpl.html>
+ * 		Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+class HighChartsAppController extends AppController {
+    public function beforeFilter() {
+        parent::beforeFilter();	  	
+        $this->layout = 'HighCharts.chart.demo';
+    }
+}
---
+++ Model/HighchartsAppModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  CakePHP HighCharts Plugin
+ * 
+ * 	Copyright (C) 2012 Kurn La Montagne / destinydriven
+ *	<https://github.com/destinydriven> 
+ * 
+ * 	Multi-licensed under:
+ * 		MPL <http://www.mozilla.org/MPL/MPL-1.1.html>
+ * 		LGPL <http://www.gnu.org/licenses/lgpl.html>
+ * 		GPL <http://www.gnu.org/licenses/gpl.html>
+ * 		Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+class HighChartsAppModel extends AppModel {
+}
